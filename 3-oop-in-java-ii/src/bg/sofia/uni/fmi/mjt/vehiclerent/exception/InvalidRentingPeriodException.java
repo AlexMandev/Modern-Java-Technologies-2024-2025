8c75a7af9e83beb95e5979a9f3a4dab9ede45308
AlexMandev <68728983+AlexMandev@users.noreply.github.com>
2024-11-25 16:14:32 +0200

    Delete 3-oop-in-java-ii directory

--- 3-oop-in-java-ii/src/bg/sofia/uni/fmi/mjt/vehiclerent/exception/InvalidRentingPeriodException.java
+++
@@ -1,7 +0,0 @@
-package bg.sofia.uni.fmi.mjt.vehiclerent.exception;
-
-public class InvalidRentingPeriodException extends Exception {
-    public InvalidRentingPeriodException(String message) {
-        super(message);
-    }
-}
